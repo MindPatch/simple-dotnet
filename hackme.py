616ff890ed668947bd0338c01ff0b5176c109076
Khaled <45688522+MindPatch@users.noreply.github.com>
2024-12-01 01:23:29 +0200

    Update hackme.py

--- hackme.py
+++ hackme.py
@@ -8,7 +8,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
 from contextlib import closing# teast
 from jinja2 import Environment
 # configuration test
-DATABASE = 'hackme.db'# beat
+DATABASE = 'hackme.db'# beat # tester
 DEBUG = True
 SECRET_KEY = 'development key' 
 USERNAME = 'admin'#test# test 
